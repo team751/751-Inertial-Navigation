3bfd418c2571d3eb8a40e36b2fa31b85d8e823fc
Sam Crow <sam.crow@liquidr.com>
2012-07-18 15:04:24 -0700

    Fixed exception handling

--- src/org/team751/inav/INavDataProvider.java
+++ src/org/team751/inav/INavDataProvider.java
@@ -13,15 +13,19 @@ public interface INavDataProvider {
 	 * 360 or less than zero.
 	 * 
 	 * @return The robot's heading
+	 * @throws InterruptedException if the thread was interrupted while
+	 * waiting for another thread to unlock the mutex on a necessary object
 	 */
-	public double getHeading();
+	public double getHeading() throws InterruptedException;
 	
 	/**
 	 * Get the acceleration of the robot along its longitudinal (front-back) axis
 	 * in meters per second squared. This value may come from an accelerometer
 	 * or from drivetrain encoders.
 	 * @return The robot's acceleration.
+	 * @throws InterruptedException if the thread was interrupted while
+	 * waiting for another thread to unlock the mutex on a necessary object
 	 */
-	public double getAcceleration();
+	public double getAcceleration() throws InterruptedException;
 
 }
